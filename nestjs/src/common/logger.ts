b64163197c5f6df1418cdef3d1f1c29b7be76555
hunjuly <hunjuly@gmail.com>
2022-05-31 00:39:39 +0000

    .

--- nestjs/src/common/logger.ts
+++ nestjs/src/common/logger.ts
@@ -1,6 +1,5 @@
 import { Logger } from '@nestjs/common'
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common'
-import { response } from 'express'
 import { Observable } from 'rxjs'
 import { tap } from 'rxjs/operators'
 
