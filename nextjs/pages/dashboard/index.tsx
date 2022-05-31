7d24750c32d70ece1c899ad6bf403b053d390782
hunjuly <hunjuly@gmail.com>
2022-05-31 10:54:45 +0000

    .

--- nextjs/pages/dashboard/index.tsx
+++ nextjs/pages/dashboard/index.tsx
@@ -7,9 +7,19 @@ import { Box } from '@mui/material'
 import { FetchError } from 'lib/types'
 
 /*
+api는 클라이언트에서,정적 렌더링만
+ssr은 데이터 다 구해서 전달
+ssr의 세션은?
+
+1. mui 제거, 기본 css로 구현, nextjs만 사용, nextjs 샘플만 사용한다.
 0. SSR 구현
 1. Dashboard에 users 표시
 2. Layout 구현
+
+1. mui는 data를 기존 mui에 맞춘다. MuiList에 MuiItem이 있다면 거기에 맞춰서 ssr 단계에서 채워넣는다.
+
+공연 목록
+제목, 시간, 티켓, 예매
 */
 export default function Dashboard() {
     const { user, mutateUser } = useUser({ redirectTo: '/signin' })
