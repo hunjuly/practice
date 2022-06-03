4f60807c21f039c7057b527c52468fc4a8f6504a
hunjuly <hunjuly@gmail.com>
2022-06-03 00:00:55 +0000

    request func -> class Request

--- nextjs/lib/request.ts
+++ nextjs/lib/request.ts
@@ -32,51 +32,55 @@ export class FetchError extends Error {
     }
 }
 
-export async function get(path: string, authCookie?: string): Promise<ResponseType> {
-    const headers = authCookie ? { cookie: authCookie } : undefined
+class RequestRest {
+    constructor(private readonly hostUrl = '') {}
 
-    return request(path, { method: 'GET', headers })
-}
+    public async get(path: string, authCookie?: string): Promise<ResponseType> {
+        const headers = authCookie ? { cookie: authCookie } : undefined
 
-export async function delete_(path: string, authCookie?: string): Promise<ResponseType> {
-    const headers = authCookie ? { cookie: authCookie } : undefined
-
-    return request(path, { method: 'DELETE', headers })
-}
-
-export async function post(path: string, body: unknown, authCookie?: string): Promise<ResponseType> {
-    const headers = authCookie ? { cookie: authCookie } : undefined
-
-    const option = {
-        method: 'POST',
-        headers: { ...headers, 'Content-Type': 'application/json' },
-        body: JSON.stringify(body)
+        return this.request(path, { method: 'GET', headers })
     }
 
-    return request(path, option)
-}
+    public async delete_(path: string, authCookie?: string): Promise<ResponseType> {
+        const headers = authCookie ? { cookie: authCookie } : undefined
 
-async function request(path: string, init?: RequestInit): Promise<ResponseType> {
-    const backendUrl = process.env.BACKEND_URL as string
+        return this.request(path, { method: 'DELETE', headers })
+    }
 
-    console.log('REQUEST - ', path, init?.body, init?.headers)
+    public async post(path: string, body: unknown, authCookie?: string): Promise<ResponseType> {
+        const headers = authCookie ? { cookie: authCookie } : undefined
 
-    if (backendUrl === 'mock') {
-        return requestMock(path, init)
-    } else {
-        const url = backendUrl + path
-
-        const response = await fetch(url, init)
-
-        const data = await response.json()
-
-        if (response.ok) {
-            console.log('RESPONSE OK - ', data, response.headers)
-            return { data, headers: response.headers }
+        const option = {
+            method: 'POST',
+            headers: { ...headers, 'Content-Type': 'application/json' },
+            body: JSON.stringify(body)
         }
 
-        console.log('RESPONSE ERROR- ', response.statusText)
+        return this.request(path, option)
+    }
 
-        throw new FetchError({ message: response.statusText, response, data })
+    public async request(path: string, init?: RequestInit): Promise<ResponseType> {
+        console.log('REQUEST - ', path, init?.body, init?.headers)
+
+        if (this.hostUrl === 'mock') {
+            return requestMock(path, init)
+        } else {
+            const url = this.hostUrl + path
+
+            const response = await fetch(url, init)
+
+            const data = await response.json()
+
+            if (response.ok) {
+                console.log('RESPONSE OK - ', data, response.headers)
+                return { data, headers: response.headers }
+            }
+
+            console.log('RESPONSE ERROR- ', response.statusText)
+
+            throw new FetchError({ message: response.statusText, response, data })
+        }
     }
 }
+
+const backendUrl = process.env.BACKEND_URL as string
