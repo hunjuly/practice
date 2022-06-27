7fa3355925965469888361b55162da1c3a8d60ab
hunjuly <hunjuly@gmail.com>
2022-06-27 04:45:49 +0000

    .

--- nestjs/src/global/global.module.ts
+++ nestjs/src/global/global.module.ts
@@ -30,6 +30,10 @@ import { MyLogger } from './my-logger'
             provide: APP_INTERCEPTOR,
             useClass: LoggingInterceptor
         },
+        // useClass 대신에 useExisting을 사용하는 이유
+        // e2e 테스트에서 UserGuard를 mocking한다.
+        // useExisting을 해야 클래스를 mock로 교체할 수 있다.
+        // https://docs.nestjs.com/fundamentals/testing#overriding-globally-registered-enhancers
         {
             provide: APP_GUARD,
             useExisting: UserGuard
