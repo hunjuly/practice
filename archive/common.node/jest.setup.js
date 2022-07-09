92bc7e728e81d784c9615e7131b75264d9452a19
hunjuly <hunjuly@gmail.com>
2022-07-09 19:56:43 +0000

    .

--- archive/common.node/jest.setup.js
+++
@@ -1,7 +0,0 @@
-import * as fs from 'fs'
-
-process.env['SERVICE_PORT'] = '4000'
-
-if (fs.existsSync('build')) {
-    fs.rmSync('build', { recursive: true })
-}
--- archive/performance/stress.sh
+++
@@ -1,3 +0,0 @@
-ab -k -n 90000 -c 10 'http://localhost:3000/v2/stress/write'
-curl http://localhost:3000/v2/status
-node --prof-process v8.log >processed.txt
