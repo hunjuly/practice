f405974880b1dac695a18af62e7b32cf60381d90
hunjuly <hunjuly@gmail.com>
2022-07-18 14:46:00 +0000

    .

---
+++ archive/nextjs/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Router from 'next/router'
+
+export default function Home() {
+    React.useEffect(() => {
+        Router.push('/dashboard')
+    })
+}
--- nextjs/pages/_app.tsx
+++ nextjs/pages/_app.tsx
@@ -1,40 +1,8 @@
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
-import { AppProps } from 'next/app'
-import { SWRConfig } from 'swr'
-import { clientSide } from 'lib/request'
-import './globals.css'
-import { UserContext, useUserContext } from '../context/UserContext'
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-    const user = useUserContext()
-
-    const getLayout =
-        Component.getLayout ??
-        ((page) => {
-            return <div>GLOBAL LAYOUT {page}</div>
-        })
-
-    return getLayout(
-        <UserContext.Provider value={user}>
-            <SWRConfig
-                value={{
-                    fetcher: (path: string) => clientSide.get(path),
-                    onError: (err) => {
-                        console.error(err)
-                    }
-                }}
-            >
-                <Component {...pageProps} />
-            </SWRConfig>
-        </UserContext.Provider>
-    )
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
-type NextPageWithLayout = NextPage & {
-    getLayout?: (page: ReactElement) => ReactNode
-}
-
-type AppPropsWithLayout = AppProps & {
-    Component: NextPageWithLayout
-}
+export default MyApp
