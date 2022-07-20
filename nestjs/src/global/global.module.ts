e2a1cda2b765eb194c9dcd1fa9744b8e5463209f
hunjuly <hunjuly@gmail.com>
2022-07-20 09:50:22 +0000

    .

--- nestjs/src/global/global.module.ts
+++ nestjs/src/global/global.module.ts
@@ -2,7 +2,7 @@ import { APP_PIPE } from '@nestjs/core'
 import { Module, ValidationPipe } from '@nestjs/common'
 import { SessionModule } from './session.module'
 import { createConfigModule } from './config.module'
-import { createOrmModule } from './orm.module'
+import { createOrmModule } from './orm'
 import { LoggerModule } from './logger'
 import { AuthModule } from './auth'
 
---
+++ nestjs/src/global/orm/orm.module.ts
@@ -0,0 +1,75 @@
+import 'dotenv/config'
+import { Injectable } from '@nestjs/common'
+import { TypeOrmModule, TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm'
+import { exit } from 'process'
+import { ConfigService } from '@nestjs/config'
+import { Logger } from '@nestjs/common'
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions'
+import { createLogger } from '../logger'
+import { OrmLogger } from './orm-logger'
+
+@Injectable()
+class TypeOrmConfigService implements TypeOrmOptionsFactory {
+    private logger: OrmLogger
+
+    async onModuleInit() {}
+    async onModuleDestroy() {
+        await this.logger.close()
+    }
+
+    constructor(private config: ConfigService) {
+        const option = {
+            storagePath: config.get<string>('LOG_STORAGE_PATH'),
+            storageDays: config.get<number>('LOG_STORAGE_DAYS'),
+            fileLevel: config.get<string>('LOG_FILE_LEVEL'),
+            consoleLevel: config.get<string>('LOG_CONSOLE_LEVEL'),
+            context: 'orm'
+        }
+
+        const winston = createLogger(option)
+
+        this.logger = new OrmLogger(winston)
+    }
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        const nodeEnv = this.config.get<string>('NODE_ENV')
+        const synchronize = this.config.get<boolean>('TYPEORM_ENABLE_SYNC')
+
+        if (nodeEnv === 'production' && synchronize) {
+            Logger.error('Do not use synchronize(TYPEORM_ENABLE_SYNC) on production')
+
+            exit(1)
+        }
+
+        type DatabaseType = 'mysql' | 'sqlite' | undefined
+
+        const common = {
+            type: this.config.get<DatabaseType>('TYPEORM_TYPE'),
+            synchronize,
+            autoLoadEntities: true,
+            logger: this.logger,
+            database: this.config.get<string>('TYPEORM_DATABASE')
+        }
+
+        if (common.type === 'sqlite') {
+            Logger.warn('database connection is not set. using MEMORY DB.')
+
+            return common as TypeOrmModuleOptions
+        } else if (common.type === 'mysql') {
+            const host = this.config.get<string>('TYPEORM_HOST')
+            const port = this.config.get<number>('TYPEORM_PORT')
+            const username = this.config.get<string>('TYPEORM_USERNAME')
+            const password = this.config.get<string>('TYPEORM_PASSWORD')
+
+            return { ...common, host, port, username, password } as MysqlConnectionOptions
+        }
+
+        throw new Error(`unknown TYPEORM_TYPE(${common.type})`)
+    }
+}
+
+export async function createOrmModule() {
+    return TypeOrmModule.forRootAsync({
+        useClass: TypeOrmConfigService
+    })
+}
