8b97e52657a261a3e32ae87f96cf9585087b8336
hunjuly <hunjuly@gmail.com>
2022-07-20 01:06:59 +0000

    .

--- archive/nextjs/test.sh
+++
@@ -1,3 +0,0 @@
-curl http://localhost:3000/api/signup -d '{ "email": "test@mail.com", "password": "testpass" }' -H "Content-Type: application/json"
-
-curl http://localhost:3000/api/login -d '{ "email": "test@mail.com", "password": "testpass" }' -H "Content-Type: application/json" -v
